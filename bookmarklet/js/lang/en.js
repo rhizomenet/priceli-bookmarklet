3b8ac714856527444f65db286943fdbae915ef60
rhizomenet <dev@rhizomenet.com>
2015-07-13 08:50:46 +0300

    Create en.js

---
+++ bookmarklet/js/lang/en.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright © 2009-2014 Rhizome Networks LTD All rights reserved.
+ *
+ * Created by  Tomer Schilman on 17/06/2015.
+ */
+
+/**
+ * This is the language file.  with this files the GUI can be customize t display text with the wanted language.
+ * This is the english language file.
+ */
+cg.en = function en() {
+    this.StoreNotFoundError = "Store isn't supported yet!";
+
+    this.ItemPriceHeader = "Price";
+    this.ItemStoreHeader = "Branch";
+    this.ItemBranchHeader = "Store";
+    this.CityAndAddressUnknown = "Address hasn't been given!";
+};
