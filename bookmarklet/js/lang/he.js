5c93aee9f90725ae0915d9ec6f52b5ff1f5962c0
rhizomenet <dev@rhizomenet.com>
2015-07-13 08:51:17 +0300

    Create he.js

---
+++ bookmarklet/js/lang/he.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright © 2009-2014 Rhizome Networks LTD All rights reserved.
+ *
+ * Created by  Tomer Schilman on 17/06/2015.
+ *
+ */
+
+/**
+ * This is the language file.  with this files the GUI can be customize t display text with the wanted language.
+ * This is the hebrew language file.
+ */
+cg.he = function he() {
+    this.StoreNotFoundError = "החנות לא נתמכת!";
+
+    this.ItemPriceHeader = "מחיר";
+    this.ItemStoreHeader = "חנות";
+    this.ItemBranchHeader = "סניף";
+    this.CityAndAddressUnknown = "לא ניתנה כתובת";
+};
